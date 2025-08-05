0d35b717bd0f0d2e9df76aae43adc66fbe330e38
REHEATZ <168640923+REHEATZ@users.noreply.github.com>
2025-08-05 13:37:06 +0100

    initial upload

--- data.js
+++ data.js
@@ -1,178 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-new-hot-well",
-      "name": "New Hot Well",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1440,
-      "initialViewParameters": {
-        "yaw": -0.5227864824008002,
-        "pitch": 1.2479348037497342,
-        "fov": 1.3755359841548254
-      },
-      "linkHotspots": [
-        {
-          "yaw": 2.5192649077652245,
-          "pitch": 0.29428952635894134,
-          "rotation": 12.566370614359176,
-          "target": "3-full-scale-system-"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": -1.4831732560451805,
-          "pitch": 0.8198343541955477,
-          "title": "Hot Well",
-          "text": "This hot well was built in collaboration with Build-It International, our capacity building and educational partner. Hot water from boiler blowdowns enters through these pipes and flows over a series of steel heat exchangers for WWHR.&nbsp;"
-        },
-        {
-          "yaw": 0.4207627650569332,
-          "pitch": 0.8046966608844528,
-          "title": "Fresh Water Pipes",
-          "text": "These pipes carry cold water into the hot well and WWHR-heated water out! You can follow the pipes through the system and see how thermal energy is reused in the Zambeef boiler.&nbsp;"
-        },
-        {
-          "yaw": -2.1994207708341236,
-          "pitch": -0.05440689542307453,
-          "title": "Boiler House",
-          "text": "This is where Zambeef's coal boiler is housed. Placing the REHEATZ system near the boiler minimises heat loss to the environment.&nbsp;"
-        }
-      ]
-    },
-    {
-      "id": "1-flowmeter-and-laptop",
-      "name": "Flowmeter and Laptop",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1440,
-      "initialViewParameters": {
-        "yaw": 0,
-        "pitch": 0,
-        "fov": 1.3755359841548254
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.27781067741017473,
-          "pitch": 0.45226206374896094,
-          "rotation": 3.141592653589793,
-          "target": "3-full-scale-system-"
-        },
-        {
-          "yaw": 1.0307056382853368,
-          "pitch": -0.24460662419551582,
-          "rotation": 0,
-          "target": "2-pipeworks"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": -0.25307439137134224,
-          "pitch": -0.3960849934625479,
-          "title": "Digital Flowmeter",
-          "text": "This flowmeter records the temperature and speed of the pre-treated cold water flowing into the hot well. By tracking these volumes and the temperatures in the hot well, we're able to calculate the heat recovery taking place in the system.&nbsp;"
-        },
-        {
-          "yaw": -1.6452280819681206,
-          "pitch": 0.1967587914065465,
-          "title": "Data Collection",
-          "text": "In addition to the TinyTag sensors, we have a laptop on-site to record flowmeter data. REHEATZ team members visit the site every two weeks to make sure everything is running smoothly and collect data for analysis.&nbsp;"
-        }
-      ]
-    },
-    {
-      "id": "2-pipeworks",
-      "name": "Pipeworks",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 1440,
-      "initialViewParameters": {
-        "yaw": -0.06778535646795447,
-        "pitch": -0.014779506535848697,
-        "fov": 1.3755359841548254
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.6297296248422555,
-          "pitch": -0.014105864475197905,
-          "rotation": 1.5707963267948966,
-          "target": "1-flowmeter-and-laptop"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": 2.940766138535025,
-          "pitch": 0.1596190938931823,
-          "title": "TinyTags",
-          "text": "This is one of our many TinyTag sensors on-site. This one measures the temperature of water going to the boiler, after heat recovery has taken place.&nbsp;"
-        },
-        {
-          "yaw": 0.1275131115218393,
-          "pitch": -0.4778469359812707,
-          "title": "TinyTag",
-          "text": "This TinyTag measures the temperature of water going into the storage tank, before heat recovery has taken place.&nbsp;"
-        },
-        {
-          "yaw": -0.9462458999497514,
-          "pitch": -0.3136620370551757,
-          "title": "Water Treatment Plant",
-          "text": "This is where Zambeef treats their groundwater before use on-site. Originally, groundwater was used in the WWHR system and was then sent for treatment; now, the water in the WWHR system has already been softened and is ready for direct use in the boiler.&nbsp;"
-        }
-      ]
-    },
-    {
-      "id": "3-full-scale-system-",
+      "id": "0-full-scale-system-",
       "name": "Full-Scale System ",
       "levels": [
         {
@@ -195,8 +24,8 @@ var APP_DATA = {
       ],
       "faceSize": 1440,
       "initialViewParameters": {
-        "yaw": -0.04915316297356043,
-        "pitch": 0.07287607863071344,
+        "yaw": -0.04847896024163134,
+        "pitch": -0.03682444456998368,
         "fov": 1.3755359841548254
       },
       "linkHotspots": [
@@ -204,19 +33,19 @@ var APP_DATA = {
           "yaw": 0.2846486367332268,
           "pitch": 0.2227438329077316,
           "rotation": 0,
-          "target": "1-flowmeter-and-laptop"
+          "target": "2-data-collection"
         },
         {
           "yaw": 0.4885321569721395,
           "pitch": 0.05596881934803122,
           "rotation": 0,
-          "target": "2-pipeworks"
+          "target": "3-pipeworks"
         },
         {
           "yaw": -2.9101111362644296,
           "pitch": 0.054083686197710534,
           "rotation": 0,
-          "target": "0-new-hot-well"
+          "target": "1-new-hot-well"
         }
       ],
       "infoHotspots": [
@@ -261,6 +90,177 @@ var APP_DATA = {
           "pitch": 0.15997721347005012,
           "title": "Data Collection&nbsp;",
           "text": "This shelter protects some of our electronic equipment -- click on the arrows to learn more!"
+        }
+      ]
+    },
+    {
+      "id": "1-new-hot-well",
+      "name": "New Hot Well",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.5227864824008002,
+        "pitch": 1.2479348037497342,
+        "fov": 1.3755359841548254
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5192649077652245,
+          "pitch": 0.29428952635894134,
+          "rotation": 12.566370614359176,
+          "target": "0-full-scale-system-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.4831732560451805,
+          "pitch": 0.8198343541955477,
+          "title": "Hot Well",
+          "text": "This hot well was built in collaboration with Build-It International, our capacity building and educational partner. Hot water from boiler blowdowns enters through these pipes and flows over a series of steel heat exchangers for WWHR.&nbsp;"
+        },
+        {
+          "yaw": 0.4207627650569332,
+          "pitch": 0.8046966608844528,
+          "title": "Fresh Water Pipes",
+          "text": "These pipes carry cold water into the hot well and WWHR-heated water out! You can follow the pipes through the system and see how thermal energy is reused in the Zambeef boiler.&nbsp;"
+        },
+        {
+          "yaw": -2.1994207708341236,
+          "pitch": -0.05440689542307453,
+          "title": "Boiler House",
+          "text": "This is where Zambeef's coal boiler is housed. Placing the REHEATZ system near the boiler minimises heat loss to the environment.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "2-data-collection",
+      "name": "Data Collection",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3755359841548254
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.27781067741017473,
+          "pitch": 0.45226206374896094,
+          "rotation": 3.141592653589793,
+          "target": "0-full-scale-system-"
+        },
+        {
+          "yaw": 1.0307056382853368,
+          "pitch": -0.24460662419551582,
+          "rotation": 0,
+          "target": "3-pipeworks"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.25307439137134224,
+          "pitch": -0.3960849934625479,
+          "title": "Digital Flowmeter",
+          "text": "This flowmeter records the temperature and speed of the pre-treated cold water flowing into the hot well. By tracking these volumes and the temperatures in the hot well, we're able to calculate the heat recovery taking place in the system.&nbsp;"
+        },
+        {
+          "yaw": -1.6452280819681206,
+          "pitch": 0.1967587914065465,
+          "title": "Data Collection",
+          "text": "In addition to the TinyTag sensors, we have a laptop on-site to record flowmeter data. REHEATZ team members visit the site every two weeks to make sure everything is running smoothly and collect data for analysis.&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "3-pipeworks",
+      "name": "Pipeworks",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.06778535646795447,
+        "pitch": -0.014779506535848697,
+        "fov": 1.3755359841548254
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6297296248422555,
+          "pitch": -0.014105864475197905,
+          "rotation": 1.5707963267948966,
+          "target": "2-data-collection"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.940766138535025,
+          "pitch": 0.1596190938931823,
+          "title": "TinyTags",
+          "text": "This is one of our many TinyTag sensors on-site. This one measures the temperature of water going to the boiler, after heat recovery has taken place.&nbsp;"
+        },
+        {
+          "yaw": 0.1275131115218393,
+          "pitch": -0.4778469359812707,
+          "title": "TinyTag",
+          "text": "This TinyTag measures the temperature of water going into the storage tank, before heat recovery has taken place.&nbsp;"
+        },
+        {
+          "yaw": -0.9462458999497514,
+          "pitch": -0.3136620370551757,
+          "title": "Water Treatment Plant",
+          "text": "This is where Zambeef treats their groundwater before use on-site. Originally, groundwater was used in the WWHR system and was then sent for treatment; now, the water in the WWHR system has already been softened and is ready for direct use in the boiler.&nbsp;"
         }
       ]
     }
